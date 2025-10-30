e8c3f450d9ec86ee3d4873318fa4637ad9556c59
keem-hyun <hw813@naver.com>
2025-10-30 18:23:51 +0900

    add: app color & text style

--- lib/core/design_system/colors/app_colors.dart
+++ lib/core/design_system/colors/app_colors.dart
@@ -8,4 +8,17 @@ class AppColors {
   static const Color black = Color(0xFF111111);
   static const Color white = Color(0xFFFFFFFF);
   static const Color gray = Color(0xFF616772);
+  static const Color darkGray = Color(0xFF666666);
+  
+  static const Color logo = Color(0xFF8A56E2);
+  static const Color brandText = Color(0xFF7250C9);
+
+  // 로그인 화면 컬러
+  static const Color kakaoYellow = Color(0xFFFEE500);
+  static const Color appleGray = Color(0xFFEBEFF5);
+
+  // 기타
+  static const Color placeholderGray = Color(0xFFD9D9D9);
+  static const Color descGray = Color(0xFF9BA3B1);
+  
 }
--- lib/core/design_system/typography/app_text_styles.dart
+++ lib/core/design_system/typography/app_text_styles.dart
@@ -15,4 +15,53 @@ class AppTextStyles {
     height: 1.5,
     letterSpacing: -0.9,
   );
+
+  // 브랜드 스타일
+  static const TextStyle brandName = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.45,
+  );
+
+  // 로그인 화면 스타일
+  static const TextStyle loginMainTitle = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 36,
+    fontWeight: FontWeight.w700,
+    height: 1.5,
+    letterSpacing: -0.9,
+  );
+
+  static const TextStyle loginSubtitle = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: -0.4,
+  );
+
+  static const TextStyle socialLoginButton = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: -0.4,
+  );
+
+  static const TextStyle legalText = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: -0.35,
+  );
+
+  static const TextStyle businessInfo = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+    letterSpacing: -0.35,
+  );
 }
